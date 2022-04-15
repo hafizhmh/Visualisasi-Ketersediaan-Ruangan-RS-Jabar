9df1c057da4216c8ad5dba9f73c8b20aa806a455
hmulya <hmulya@fintax.id>
2022-04-15 21:12:10 +0700

    change to full dataset

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import altair as alt
 st.set_page_config(layout="wide")
 # st.title('Halo Wil')
 st.title('Data Ketersediaan Kasur di Rumah Sakit Jawa Barat')
-DATA_URL = ('satgas-covid-19-dp_cvd_bor_data_wilayah_data')
+DATA_URL = ('satgas-covid-19-dp_cvd_bor_data_wilayah_data.csv')
 colsize=[3.2,5]
 
 @st.cache
